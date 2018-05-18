2d25add4b506c18866cf574ee4ce03defa25216f
Bryan Belanger <bryan@autostructure.io>
2018-05-18 16:01:27 -0400

    [blacksmith] Bump version to 1.0.13

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -14,7 +14,7 @@ class laravel::configure {
     line   => 'cgi.fix_pathinfo=0',
   }
 
-  file { '/etc/nginx/default.d/default':
+  file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/laravel/default',
   }
